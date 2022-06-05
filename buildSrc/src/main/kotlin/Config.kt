3480653b99b3c063ca4d08fb55869df991414702
Egor Zhir <zhirokegor@gmail.com>
2022-06-05 16:54:35 +0300

    feat: increased version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val packageName = "com.zhiroke.reminder"
     const val minSdk = 26
     const val targetSdk = 31
-    const val versionCode = 4
+    const val versionCode = 200000
     const val versionName = "2.0.0"
 
 }
